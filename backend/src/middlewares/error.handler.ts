0783b0601dfabed5e6407703f506d997853b2443
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-01-30 19:57:41 +0800

    Fix cannot find type definitions in interface

--- backend/src/middlewares/error.handler.ts
+++ backend/src/middlewares/error.handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import env from "../utilities/env";
-import { ErrorResponse } from "../interfaces/error.props";
+import { ErrorResponse } from "@interfaces/error.props";
 
 export const NotFound = (req: Request, res: Response, next: NextFunction) => {
   // * Set HTTP status code to 404 (Not Found)
