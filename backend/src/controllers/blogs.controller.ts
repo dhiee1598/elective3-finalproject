f46ed8814f19da26babde274eccd91641ba78d7e
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-02-12 23:32:30 +0800

    update blog controller

--- backend/src/controllers/blogs.controller.ts
+++ backend/src/controllers/blogs.controller.ts
@@ -1,24 +1,54 @@
 import { RequestHandler } from "express";
 import asyncHandler from "express-async-handler";
-import { BlogsNewRequest, BlogsResponse } from "../interfaces/blogs.props";
-import { FetchAllBlogsByUser, InsertBlogs } from "../services/blogs.service";
+import {
+  BlogsNewRequest,
+  BlogsParams,
+  BlogsResponse,
+} from "../interfaces/blogs.props";
+import {
+  FetchAllBlogs,
+  FetchSingleBlogsByID,
+  InsertBlogs,
+} from "../services/blogs.service";
 
-export const GetAllBlogsUsers: RequestHandler<
+export const GetAllBlogs: RequestHandler<
   unknown,
   BlogsResponse,
   unknown,
   unknown
-> = asyncHandler(async (req, res) => {
-  // Retrieve the user ID from the request (authenticated user)
-  const id = req.userId;
+> = asyncHandler(async (_req, res) => {
+  // * Fetch all blogs
+  const blogs = await FetchAllBlogs();
 
-  // Fetch all blogs that belong to the current user
-  const blogs = await FetchAllBlogsByUser(id);
-
-  // Return a successful response with the fetched blogs
+  // * Return a successful response with the fetched blogs
   res.status(200).json({
-    message: "Successfully retrieved all blogs for user",
+    message: "Successfully retrived all blogs",
     blogs: blogs,
+  });
+});
+
+export const GetSingleBlogs: RequestHandler<
+  BlogsParams,
+  BlogsResponse,
+  unknown,
+  unknown
+> = asyncHandler(async (req, res) => {
+  // * Extracting blogId from request parameters
+  const blogId = req.params.blogId;
+
+  // * Fetching the blog data by ID
+  const singleBlog = await FetchSingleBlogsByID(blogId);
+
+  // ! If the blog does not exist, return a 404 error
+  if (!singleBlog) {
+    res.status(404);
+    throw new Error("Blogs Not Found");
+  }
+
+  // * Send the response with the retrieved blog data
+  res.status(200).json({
+    message: "Successfully retrieved single blog",
+    blogs: singleBlog,
   });
 });
 
@@ -28,10 +58,10 @@ export const CreateNewBlogs: RequestHandler<
   BlogsNewRequest,
   unknown
 > = asyncHandler(async (req, res) => {
-  // Retrieve the user ID from the request (authenticated user)
+  // * Retrieve the user ID from the request (authenticated user)
   const id = req.userId;
 
-  // Insert the new blog into the database with the provided blog data and user ID
+  // * Insert the new blog into the database with the provided blog data and user ID
   const newBlogs = await InsertBlogs(req.body, id);
 
   res.status(201).json({
