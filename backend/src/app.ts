b38551f807940cb2ad0fab0dc489cdcfaf98ba03
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-02-15 02:44:45 +0800

    commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response } from "express";
-// import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-// import env from "./utilities/env";
+import env from "./utilities/env";
 import { NotFound, ErrorHandler } from "./middlewares/error.handler";
-// import UserRoutes from "./routes/users.route";
-// import TokenRoutes from "./routes/token.route";
-// import BlogRoutes from "./routes/blogs.route";
-// import RequiredAuthentication from "./middlewares/auth";
+import UserRoutes from "./routes/users.route";
+import TokenRoutes from "./routes/token.route";
+import BlogRoutes from "./routes/blogs.route";
+import RequiredAuthentication from "./middlewares/auth";
 
-const ExpressConfig = () => {
+const ExpressConfig = (): Application => {
   const app = express();
 
   // * MIDDLEWARE SETUP
@@ -18,26 +18,26 @@ const ExpressConfig = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
-  // app.use(
-  //   cors({
-  //     credentials: true,
-  //     origin: env.FRONTEND_URL,
-  //   }),
-  // );
+  app.use(
+    cors({
+      credentials: true,
+      origin: env.FRONTEND_URL,
+    }),
+  );
 
   // * MAIN HEALTH CHECK ROUTE
-  app.get("/", (_req: Request, res: Response) => {
+  app.get("/main/healthcheck", (_req: Request, res: Response) => {
     res.status(200).json({ message: "HAPPY CODING - 👋✨🌍" });
   });
 
-  // // * API ROUTES FOR TOKEN
-  // app.use("/api/auth/token", TokenRoutes);
-  //
-  // // * API ROUTES FOR USERS
-  // app.use("/api/users", UserRoutes);
-  //
-  // // * API ROUTES FOR BLOGS
-  // app.use("/api/blogs", RequiredAuthentication, BlogRoutes);
+  // * API ROUTES FOR TOKEN
+  app.use("/api/auth/token", TokenRoutes);
+
+  // * API ROUTES FOR USERS
+  app.use("/api/users", UserRoutes);
+
+  // * API ROUTES FOR BLOGS
+  app.use("/api/blogs", RequiredAuthentication, BlogRoutes);
 
   // ! CATCH ALL ERROR HANDLING
   app.use(NotFound); // Handle 404 errors (Not Found)
