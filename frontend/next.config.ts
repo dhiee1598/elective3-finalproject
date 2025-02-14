066acdae77b7c863398a4bf403e992daf38810e7
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-02-15 01:23:48 +0800

    commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true, // Important for correct routing
+  /* config options here */
   env: {
     BACKEND_SERVER_URL: process.env.BACKEND_SERVER_URL,
   },
-  output: "standalone", // Helpful for deployment on Vercel
 };
 
 export default nextConfig;
