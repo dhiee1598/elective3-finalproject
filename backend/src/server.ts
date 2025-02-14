08f0962a79bf9923c376544fb38ef6fe22f46e5a
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-02-15 02:32:00 +0800

    commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,20 +4,6 @@ import env from "./utilities/env";
 
 const app = ExpressConfig();
 
-const StartServer = async (): Promise<void> => {
-  try {
-    // * Sync the Sequelize connection with the database to ensure models are updated
-    // await sequelize.sync();
-    // console.log("Connection has been established successfully");
-
-    // * Start the server and listen on the specified port
-    app.listen(env.PORT, () => {
-      console.log(`Listening: http://localhost:${env.PORT}`);
-    });
-  } catch (error) {
-    // ! Log an error message if unable to connect to the database
-    console.log(`Unable to connect to the database. ${error}`);
-  }
-};
-
-StartServer();
+app.listen(env.PORT, () => {
+  console.log(`Listening: http://localhost:${env.PORT}`);
+});
