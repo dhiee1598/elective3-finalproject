4ed8e921b70c69409a38928a01c096ad38425619
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-02-15 16:21:38 +0800

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,11 +4,13 @@ import sequelize from "./utilities/sequelize";
 
 const app = ExpressConfig();
 
+// * Sync the Sequelize connection with the database
 sequelize
   .sync()
   .then(() => console.log("Connection has been established successfully"))
   .catch((error) => console.log(`Unable to connect to the database. ${error}`));
 
+// * Start the server and listen on the specified port
 app.listen(env.PORT, () => {
   console.log(`Listening: http://localhost:${env.PORT}`);
 });
