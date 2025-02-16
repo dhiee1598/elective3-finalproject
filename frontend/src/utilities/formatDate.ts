de342193e5a5f7ab9b20a9ff5a84441223484b1e
airah <airahpadecio9@gmail.com>
2025-02-16 17:02:11 +0800

    add formatedate utilities

---
+++ frontend/src/utilities/formatDate.ts
@@ -0,0 +1,11 @@
+export const FormatDate = (dateTime: string) => {
+  return new Date(dateTime).toLocaleString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+};
